d5549fa741c35cc4ebf8f525feb1f94a87410fac
hamza elmouddane <hamzaelmouddane2000@gmail.com>
2025-07-28 06:11:40 +0100

    Code Sharing App by Sean Hale

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -5,5 +5,5 @@ export default sanityClient({
   dataset: 'code_show',       
   useCdn: false,
   apiVersion: '2023-01-01',
-  token:"skuo01MovKKro4iFXPRlI84m58xSEMv1CGmVsGABt5sMZs02rX4aaD7pQFoK5LX2BZi8wF4ixDYxFpGc9CkpuQkheoVClZJDbCuE71TbWSMeDYacEFrXmWUEmvyHGgcHUV61j1lTaq0EegaIfr3SoFcxGpUIMagIEJuATWzU2GZzVUNESKos"               
+  // token: api
 })
